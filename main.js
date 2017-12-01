2d44b920c9db64c054d106d31bf75a7168c359f7
IT-MikeS <tech@croftersorganic.com>
2017-12-01 13:58:05 -0500

    Add developing extras

--- main.js
+++ main.js
@@ -1,71 +1,83 @@
 const {app, BrowserWindow, ipcMain, Menu} = require('electron');
-
+const isDevMode = require('electron-is-dev');
 const path = require('path');
 
-//require('electron-reload')(__dirname + '/public');
+if (isDevMode)
+    require('electron-reload')(__dirname + '/public');
 
 let mainWindow;
 
-function createWindow() {
-  const browserOptions = {
-    width: 800,
-    height: 600,
-    maximizeable: false,
-    icon: path.join(__dirname, '/public/img/logo.png')
-  };
+function createWindow(){
+    const browserOptions = {
+        width: 800,
+        height: 600,
+        maximizeable: false,
+        icon: path.join(__dirname, '/public/img/logo.png')
+    };
 
-  mainWindow = new BrowserWindow(browserOptions);
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+    mainWindow = new BrowserWindow(browserOptions);
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools();
+    // Open the DevTools.
+    // mainWindow.webContents.openDevTools();
 
-  let template = [{
-    label: "YouTube To MP3",
-    submenu: [
-      { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
-      { type: "separator" },
-      { label: "Quit", accelerator: "Command+Q", click: function () { app.quit(); } }
-    ]
-  }, {
-    label: "Edit",
-    submenu: [
-      { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
-      { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
-      { type: "separator" },
-      { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
-      { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
-      { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" }
-    ]
-  }, {
-    label: 'Dev Options',
-      submenu: [
-          {label: 'Open Dev Tools', click: () => {mainWindow.webContents.openDevTools()}}
-      ]
-  }
-  ];
+    let template = [{
+        label: "YouTube To MP3",
+        submenu: [
+            {label: "About Application", selector: "orderFrontStandardAboutPanel:"},
+            {type: "separator"},
+            {
+                label: "Quit", accelerator: "Command+Q", click: function () {
+                    app.quit();
+                }
+            }
+        ]
+    }, {
+        label: "Edit",
+        submenu: [
+            {label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:"},
+            {label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:"},
+            {type: "separator"},
+            {label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:"},
+            {label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:"},
+            {label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:"}
+        ]
+    }];
 
-  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+    //if developing add dev menu option to menu bar
+    if (isDevMode)
+        template.push({
+            label: 'Dev Options',
+            submenu: [
+                {
+                    label: 'Open Dev Tools', click: () => {
+                        mainWindow.webContents.openDevTools()
+                    }
+                }
+            ]
+        });
 
-  mainWindow.on('closed', function () {
-    mainWindow = null;
-  });
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
 }
 
 app.on('ready', createWindow);
 
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', function () {
-  if (mainWindow === null) {
-    createWindow();
-  }
+    if (mainWindow === null) {
+        createWindow();
+    }
 });
 
 ipcMain.on('download-file', function (e, url) {
-  mainWindow.loadURL('http:' + url);
+    mainWindow.loadURL('http:' + url);
 });
