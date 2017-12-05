ef8854ec8a1bda0833d7cb41e4ce43ccf065dc9f
IT-MikeS <tech@croftersorganic.com>
2017-12-05 13:28:30 -0500

    Commented on new download and conversion code, also cleaned up functions

--- app/containers/app.container.js
+++ app/containers/app.container.js
@@ -22,32 +22,48 @@ class AppContainer extends Component {
       userDownloadsFolder: remote.app.getPath('downloads'),
     };
 
+    // This property will be used to control the rate at which the progress bar is updated to prevent UI lag.
     this.rateLimitTriggered = false;
+
     this.startDownload = this.startDownload.bind(this);
     this.downloadFinished = this.downloadFinished.bind(this);
   }
 
-  getVideoAsMp4(urlLink, userProvidedPath, title){
+  getVideoAsMp4(urlLink, userProvidedPath, title) {
+    // Tell the user we are starting to get the video.
     this.setState({progressMessage: 'Downloading...'});
     return new Promise((resolve, reject) => {
       let fullPath = path.join(userProvidedPath, `tmp_${title}.mp4`);
+
+      // Create a reference to the stream of the video being downloaded.
       let videoObject = ytdl(urlLink, {filter: 'audioonly'});
 
       videoObject
         .on('progress', (chunkLength, downloaded, total) => {
-          if(!this.rateLimitTriggered) {
-            let newVal = Math.floor((downloaded / total) * 100).toString();
+          // When the stream emits a progress event, we capture the currently downloaded amount and the total
+          // to download, we then divided the downloaded by the total and multiply the result to get a float of
+          // the percent complete, which is then passed through the Math.floor function to drop the decimals.
+          if (!this.rateLimitTriggered) {
+            let newVal = Math.floor((downloaded / total) * 100);
             this.setState({progress: newVal});
+
+            // Set the rate limit trigger to true and set a timeout to set it back to false. This will prevent the UI
+            // from updating every few milliseconds and creating visual lag.
             this.rateLimitTriggered = true;
-            setTimeout(()=>{this.rateLimitTriggered = false}, 800);
+            setTimeout(() => {
+              this.rateLimitTriggered = false
+            }, 800);
           }
         });
 
+      // Create write-able stream for the temp file and pipe the video stream into it.
       videoObject
         .pipe(fs.createWriteStream(fullPath))
         .on('finish', () => {
+          // all of the video stream has finished piping, set the progress bar to 100% and give user pause to see the
+          // completion of step. Then we return the path to the temp file, the output path, and the desired filename.
           this.setState({progress: 100});
-          setTimeout(()=>{
+          setTimeout(() => {
             resolve({filePath: fullPath, folderPath: userProvidedPath, fileTitle: `${title}.mp3`});
           }, 1000);
         });
@@ -55,13 +71,19 @@ class AppContainer extends Component {
   }
 
   convertMp4ToMp3(paths) {
+    // Tell the user we are starting to convert the file to mp3.
     this.setState({progressMessage: 'Converting...', progress: 0});
+
     return new Promise((resolve, reject) => {
+      // reset the rate limiting trigger just encase.
       this.rateLimitTriggered = false;
+
+      // Pass ffmpeg the temp mp4 file. Set the path where is ffmpeg binary for the platform. Provided desired format.
       ffmpeg(paths.filePath)
         .setFfmpegPath(binaries.ffmpegPath())
         .format('mp3')
         .on('progress', (progress) => {
+          // Use same rate limiting as above in function "getVideoAsMp4()" to prevent UI lag.
           if (!this.rateLimitTriggered) {
             this.setState({progress: Math.floor(progress.percent)});
             this.rateLimitTriggered = true;
@@ -72,53 +94,63 @@ class AppContainer extends Component {
         })
         .output(fs.createWriteStream(path.join(paths.folderPath, sanitize(paths.fileTitle))))
         .on('end', () => {
+          // After the mp3 is wrote to the disk we set the progress to 99% the last 1% is the removal of the temp file.
           this.setState({progress: 99});
           resolve();
         })
         .run();
     });
-
-  }
-
-  async convertVideoToMp3(urlLink, userProvidedPath) {
-    try{
-      this.setState({progressMessage: 'Fetching video info...'});
-      let info = await ytdl.getInfo(urlLink);
-      let paths = await this.getVideoAsMp4(urlLink, userProvidedPath, info.title);
-      await this.convertMp4ToMp3(paths);
-      fs.unlinkSync(paths.filePath);
-      await (() => {
-        return new Promise((resolve, reject) => {
-          setTimeout(()=>{
-            this.setState({progress: 100});
-            resolve();
-          },900);
-        });
-      });
-    } catch(e) {
-      console.error(e);
-    }
   }
 
   async startDownload(id) {
+    // Reset state for each download/conversion
     this.setState({
       progress: 0,
       showProgressBar: true,
       progressMessage: '...',
     });
+
     try {
-        await this.convertVideoToMp3(id, this.state.userDownloadsFolder);
-        this.downloadFinished();
-    } catch(e) {
+      // Tell the user we are getting the video info, and call the function to do so.
+      this.setState({progressMessage: 'Fetching video info...'});
+      let info = await ytdl.getInfo(id);
+
+      // Given the id of the video, the path in which to store the output, and the video title
+      // download the video as an audio only mp4 and write it to a temp file then return
+      // the full path for the tmp file, the path in which its stored, and the title of the desired output.
+      let paths = await this.getVideoAsMp4(id, this.state.userDownloadsFolder, info.title);
+
+      // Pass the returned paths and info into the function which will convert the mp4 tmp file into
+      // the desired output mp3 file.
+      await this.convertMp4ToMp3(paths);
+
+      // Remove the temp mp4 file.
+      fs.unlinkSync(paths.filePath);
+
+      // Set the bar to 100% and give the OS about one second to get rid of the temp file.
+      await (() => {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            this.setState({progress: 100});
+            resolve();
+          }, 900);
+        });
+      });
+
+      // Signal that the download and conversion have completed and we need to tell the user about it and then reset.
+      this.downloadFinished();
+    } catch (e) {
       console.error(e);
     }
   }
 
   downloadFinished() {
+    // Make sure progress bar is at 100% and tell the user we have completed the task successfully.
     this.setState({
       progress: 100,
       progressMessage: 'Conversion successful! Resetting in 5 seconds.'
     });
+
     // Reset the progress bar to the LinkInput
     setTimeout(() => this.setState({
       showProgressBar: false
@@ -129,14 +161,16 @@ class AppContainer extends Component {
     if (this.state.showProgressBar) {
       return (
         <div>
-          <div className="outputFolderDisplay"><span>Current output folder:&nbsp;</span>{this.state.userDownloadsFolder}</div>
+          <div className="outputFolderDisplay"><span>Current output folder:&nbsp;</span>{this.state.userDownloadsFolder}
+          </div>
           <ProgressBar progress={this.state.progress} messageText={this.state.progressMessage}/>
         </div>
       );
     } else {
       return (
         <div>
-          <div className="outputFolderDisplay"><span>Current output folder:&nbsp;</span>{this.state.userDownloadsFolder}</div>
+          <div className="outputFolderDisplay"><span>Current output folder:&nbsp;</span>{this.state.userDownloadsFolder}
+          </div>
           <LinkInput startDownload={this.startDownload}/>
         </div>
       );
