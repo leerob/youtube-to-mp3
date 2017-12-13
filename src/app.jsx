4178a92d188e39bb0521455040872b43ab375b30
IT-MikeS <tech@croftersorganic.com>
2017-12-13 15:57:30 -0500

    Swapped from browserify to a webpack workflow.

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppContainer from './containers/app.container'
+import AppContainer from './containers/app.container';
+
+import './styles/global.scss';
+import './styles/smalltalk.min.css';
 
 class App extends React.Component {
   render () {
