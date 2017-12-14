8489ef979d776ac2cee6a1c8e0ab49ccbee3d4c9
IT-MikeS <tech@croftersorganic.com>
2017-12-14 08:01:53 -0500

    Rebase to leerob/youtube-to-mp3 master branch

--- app/containers/app.container.js
+++ app/containers/app.container.js
@@ -4,7 +4,6 @@ import ProgressBar from '../components/ProgressBar';
 import React, {Component} from 'react';
 
 import * as path from 'path';
-import TabControl from "../components/TabControl";
 
 const ffmpeg = window.require('fluent-ffmpeg');
 const binaries = window.require('ffmpeg-binaries');
@@ -17,13 +16,10 @@ class AppContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      inputTabSelected: true,
       showProgressBar: false,
       progress: 0,
-      progressMessage: 'No items in queue',
+      progressMessage: '',
       userDownloadsFolder: localStorage.getItem('userSelectedFolder') ? localStorage.getItem('userSelectedFolder') : remote.app.getPath('downloads'),
-      workingOnVideo: false,
-      downloadQueue: [],
     };
 
     // Signal from main process to show prompt to change the download to folder.
@@ -35,10 +31,8 @@ class AppContainer extends Component {
     // This property will be used to control the rate at which the progress bar is updated to prevent UI lag.
     this.rateLimitTriggered = false;
 
-    this.swapTab = this.swapTab.bind(this);
     this.startDownload = this.startDownload.bind(this);
     this.downloadFinished = this.downloadFinished.bind(this);
-    this.addDownloadToQueue = this.addDownloadToQueue.bind(this);
     this.changeOutputFolder = this.changeOutputFolder.bind(this);
   }
 
@@ -122,7 +116,6 @@ class AppContainer extends Component {
       progress: 0,
       showProgressBar: true,
       progressMessage: '...',
-      workingOnVideo: true,
     });
 
     try {
@@ -166,40 +159,10 @@ class AppContainer extends Component {
       progressMessage: 'Conversion successful!'
     });
 
-    let currentQueue = [...this.state.downloadQueue];
-    currentQueue.pop();
-
-    this.setState({
-      workingOnVideo: false,
-      downloadQueue: [...currentQueue]
-    });
-
-    if(this.state.downloadQueue.length > 0) {
-      setTimeout(() => {
-        this.startDownload(this.state.downloadQueue[0]);
-      }, 2000);
-    } else {
-      setTimeout(() => {
-        this.setState({
-          progress: 0,
-          progressMessage: 'No items in queue',
-        });
-      }, 2000);
-    }
-
-  }
-
-  addDownloadToQueue(id) {
-    // test url:    https://www.youtube.com/watch?v=Ssvu2yncgWU
-    let currentQueue = [...this.state.downloadQueue];
-    currentQueue.push(id);
-    this.setState({downloadQueue: [...currentQueue]});
-
-    if(this.state.downloadQueue.length > 0 && !this.state.workingOnVideo) {
-      setTimeout(() => {
-        this.startDownload(this.state.downloadQueue[0]);
-      }, 2000);
-    }
+    // Reset the progress bar to the LinkInput
+    setTimeout(() => this.setState({
+      showProgressBar: false
+    }), 2000);
   }
 
   changeOutputFolder() {
@@ -215,27 +178,12 @@ class AppContainer extends Component {
     }
   }
 
-  swapTab(isInputTab) {
-    this.setState({inputTabSelected: isInputTab});
-  }
-
   render() {
-    if (!this.state.inputTabSelected) {
-      return (
-        <div>
-          <TabControl tabSwap={this.swapTab} selecedTab={this.state.inputTabSelected} queueQty={this.state.downloadQueue.length}/>
-          <ProgressBar progress={this.state.progress} messageText={this.state.progressMessage} />
-        </div>
-      );
+    if (this.state.showProgressBar) {
+      return <ProgressBar progress={this.state.progress} messageText={this.state.progressMessage}/>;
     } else {
-      return (
-        <div>
-          <TabControl tabSwap={this.swapTab} selecedTab={this.state.inputTabSelected} queueQty={this.state.downloadQueue.length} />
-          <LinkInput startDownload={this.addDownloadToQueue} />
-        </div>
-      );
+      return <LinkInput startDownload={this.startDownload}/>;
     }
-
   }
 }
 
