47851d25897b28cebb5b3bc24e99071c8528b952
Lee Robinson <lrobinson2011@gmail.com>
2018-12-15 12:48:29 -0600

    Switch to yarn, update deps, and run Prettier

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    arrowParens: 'always',
+    bracketSpacing: false,
+    printWidth: 120,
+    singleQuote: true,
+    tabWidth: 4
+};
--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,25 +1,29 @@
 import React, {Component} from 'react';
 
 class ProgressBar extends Component {
-  render() {
-    let percentComplete = `${this.props.progress}%`;
-    let messageToShow = `${this.props.messageText}`;
-    let color = '#747373';
+    render() {
+        let percentComplete = `${this.props.progress}%`;
+        let messageToShow = `${this.props.messageText}`;
+        let color = '#747373';
 
-    if (this.props.progress > 90) {
-      color = 'white';
+        if (this.props.progress > 90) {
+            color = 'white';
+        }
+
+        return (
+            <div>
+                <div className="progress">
+                    <div className="progress__bar" style={{width: percentComplete}} />
+                    <div className="progress__percentage" style={{color: color}}>
+                        {percentComplete}
+                    </div>
+                </div>
+                <div className="center">
+                    <span className="progress__info">{messageToShow}</span>
+                </div>
+            </div>
+        );
     }
-
-    return <div>
-      <div className='progress'>
-        <div className='progress__bar' style={{'width': percentComplete}}></div>
-        <div className='progress__percentage' style={{'color': color}}>{percentComplete}</div>
-      </div>
-      <div className='center'>
-        <span className='progress__info'>{messageToShow}</span>
-      </div>
-    </div>;
-  }
 }
 
 export default ProgressBar;
